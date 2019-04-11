7383ddf23eeaa2005e135ddaddd9ada620cbb90e
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-04-11 20:04:38 +0200

    Completed setState and delete item button introduced

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
             {
                 id: 1,
                 title: 'Take out trash',
-                completed: false
+                completed: true
             },
             {
                 id: 2,
@@ -24,10 +24,27 @@ class App extends Component {
         ]
     };
 
+    markComplete = (id) => {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo;
+            })
+        })
+    };
+
+    delTodo = (id) =>{
+        this.setState({
+            todos: [...this.state.todos.filter(todo=> todo.id !==id)]
+        })
+    };
+
     render() {
         return (
             <div className="App">
-                <Todos todos={this.state.todos}/>
+                <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
             </div>
         );
     }
